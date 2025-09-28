d27665a7b8e5fd1004ae88b01291f8f9b34c13a9
Eddu <azabacheeduardo85@gmail.com>
2025-09-28 00:01:44 -0500

    style: lint code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { SuperTokensModule } from 'supertokens-nestjs'
-import { User } from 'supertokens-node';
+import { SuperTokensModule } from 'supertokens-nestjs';
 import EmailPassword from 'supertokens-node/recipe/emailpassword';
 import Session from 'supertokens-node/recipe/session';
 import UserRoles from 'supertokens-node/recipe/userroles';
 @Module({
-    imports: [
-        SuperTokensModule.forRootAsync({
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                ...configService.get('supertokens'),
-                recipeList: [
-                    EmailPassword.init(),
-                    Session.init(),
-                    UserRoles.init()
-                ]
-
-            })
-        })
-    ]
+  imports: [
+    SuperTokensModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('supertokens'),
+        recipeList: [EmailPassword.init(), Session.init(), UserRoles.init()],
+      }),
+    }),
+  ],
 })
 export class AuthModule { }
--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
-import * as config from './env'
+import * as config from './env';
 @Module({
-    imports: [
-        NestConfigModule.forRoot({
-            envFilePath: ['.env.local', '.env'],
-            isGlobal: true,
-            load: [config.supertokensConfig]
-        })
-    ]
+  imports: [
+    NestConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'],
+      isGlobal: true,
+      load: [config.supertokensConfig],
+    }),
+  ],
 })
-export class ConfigModule { }
+export class ConfigModule {}
