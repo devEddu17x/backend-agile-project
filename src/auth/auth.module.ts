3d5d4419d0ea00818e89b7502c757d0b38bafbc5
Eddu <azabacheeduardo85@gmail.com>
2025-09-27 22:43:51 -0500

    feat(auth): integrate SuperTokens for authentication and user management
    - Added AuthModule to handle authentication logic using SuperTokens.
    - Configured SuperTokens with EmailPassword, Session, and UserRoles recipes.
    - Updated app.module.ts to include AuthModule in the imports.
    - Enhanced supertokens.config.ts to structure SuperTokens configuration.

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SuperTokensModule } from 'supertokens-nestjs'
+import { User } from 'supertokens-node';
+import EmailPassword from 'supertokens-node/recipe/emailpassword';
+import Session from 'supertokens-node/recipe/session';
+import UserRoles from 'supertokens-node/recipe/userroles';
+@Module({
+    imports: [
+        SuperTokensModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                ...configService.get('supertokens'),
+                recipeList: [
+                    EmailPassword.init(),
+                    Session.init(),
+                    UserRoles.init()
+                ]
+
+            })
+        })
+    ]
+})
+export class AuthModule { }
--- src/config/env/supertokens.config.ts
+++ src/config/env/supertokens.config.ts
@@ -1,12 +1,17 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('supertokens', () => {
+    // api key will be used later
     const { CONNECTION_URI, APP_NAME, API_DOMAIN, WEBSITE_DOMAIN, API_KEY } = process.env;
     return {
-        connectionUri: CONNECTION_URI,
-        appName: APP_NAME,
-        apiDomain: API_DOMAIN,
-        websiteDomain: WEBSITE_DOMAIN,
-        apiKey: API_KEY
+        framework: 'express',
+        supertokens: {
+            connectionURI: CONNECTION_URI,
+        },
+        appInfo: {
+            appName: APP_NAME,
+            apiDomain: API_DOMAIN,
+            websiteDomain: WEBSITE_DOMAIN,
+        }
     };
 });
