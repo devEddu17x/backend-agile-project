08b33045815dfacd9e1417bd2c0e8a2cc2a66d21
Eddu <azabacheeduardo85@gmail.com>
2025-09-27 22:25:50 -0500

    feat(config): add ConfigModule for environment configuration

---
+++ src/config/config.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as config from './env'
+@Module({
+    imports: [
+        NestConfigModule.forRoot({
+            envFilePath: ['.env.local', '.env'],
+            isGlobal: true,
+            load: [config.supertokensConfig]
+        })
+    ]
+})
+export class ConfigModule { }
