a45783943fa4b59b353a532ceed3d7eb00b90330
Eddu <azabacheeduardo85@gmail.com>
2025-09-30 00:38:54 -0500

    feat: implement session verification and role-based access for customer creation

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDTO } from './dtos/customer.dto';
+import { SuperTokensAuthGuard, VerifySession } from 'supertokens-nestjs';
+import { ROLE_NAMES } from 'src/auth/constants/roles';
 
+@UseGuards(SuperTokensAuthGuard)
 @Controller('customer')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
+  @VerifySession({
+    roles: [ROLE_NAMES.ADMIN, ROLE_NAMES.SELLER],
+  })
   @Post()
   async createCustomer(@Body() customerDTO: CreateCustomerDTO) {
     return await this.customerService.createCustomer(customerDTO);
