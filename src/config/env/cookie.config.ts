78980b5357dadaee6221af1cf2ee1ece31c783ee
Eddu <azabacheeduardo85@gmail.com>
2025-09-29 20:57:23 -0500

    refactor: streamline cookie configuration handling in session recipe (#9)

--- src/config/env/cookie.config.ts
+++ src/config/env/cookie.config.ts
@@ -4,8 +4,8 @@ export default registerAs('cookie', () => {
   const { COOKIE_DOMAIN, COOKIE_SAME_SITE, COOKIE_SECURE } = process.env;
 
   return {
-    cookieDomain: COOKIE_DOMAIN,
     cookieSameSite: COOKIE_SAME_SITE,
     cookieSecure: COOKIE_SECURE === 'true',
+    ...(COOKIE_DOMAIN ? { cookieDomain: COOKIE_DOMAIN } : {}),
   };
 });
