b43c471f3869d2e45a1fc0136c46ebebdc6437ef
Eddu <azabacheeduardo85@gmail.com>
2025-09-30 22:05:01 -0500

    fix(clothes): update imports in ClothesModule to include Gender and Size entities

--- src/clothes/clothes.module.ts
+++ src/clothes/clothes.module.ts
@@ -3,9 +3,19 @@ import { ClothesService } from './clothes.service';
 import { ClothesController } from './clothes.controller';
 import { ClothesEntity } from './entities/clothes.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClothesVariantEntity } from './entities/clothes-variant.entity';
+import { GenderEntity } from './entities/gender.entity';
+import { SizeEntity } from './entities/size.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ClothesEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      GenderEntity,
+      SizeEntity,
+      ClothesEntity,
+      ClothesVariantEntity,
+    ]),
+  ],
   providers: [ClothesService],
   controllers: [ClothesController],
 })
