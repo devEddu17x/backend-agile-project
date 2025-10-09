8210d48fabad5e7b4471ef95696b9eca73de7778
Eddu <azabacheeduardo85@gmail.com>
2025-10-08 19:03:07 -0500

    fix(customer): add getAllCustomers endpoint

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDTO } from './dtos/customer.dto';
 import { SuperTokensAuthGuard } from 'supertokens-nestjs';
 import { ROLES } from 'src/auth/constants/roles';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
+import { CustomerEntity } from './entities/customer.entity';
 
 @Roles(ROLES.SELLER)
 @UseGuards(SuperTokensAuthGuard, RolesGuard)
@@ -14,5 +15,10 @@ export class CustomerController {
   @Post()
   async createCustomer(@Body() customerDTO: CreateCustomerDTO) {
     return await this.customerService.createCustomer(customerDTO);
+  }
+
+  @Get()
+  async getAllCustomers(): Promise<CustomerEntity[]> {
+    return await this.customerService.getAllCustomers();
   }
 }
