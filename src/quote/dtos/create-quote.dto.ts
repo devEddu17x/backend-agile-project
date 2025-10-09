b95f3ed6c9b1974eaa93b036ca5e8774323f897a
Eddu <azabacheeduardo85@gmail.com>
2025-10-08 22:55:31 -0500

    feat(quote): add CreateQuoteDTO and QuoteDetailDTO for quote creation

---
+++ src/quote/dtos/create-quote.dto.ts
@@ -0,0 +1,40 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+export class CreateQuoteDTO {
+  @IsNotEmpty()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => QuoteDetailDTO)
+  details: QuoteDetailDTO[];
+
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  customerId: string;
+}
+
+export class QuoteDetailDTO {
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  clothesVariantId: string;
+
+  @IsNotEmpty()
+  @IsNumber(
+    { allowNaN: false, allowInfinity: false },
+    { message: 'quantity must be a number' },
+  )
+  @IsInt({ message: 'quantity must be an integer' })
+  @Min(1, { message: 'quantity must be at least 1' })
+  quantity: number;
+}
