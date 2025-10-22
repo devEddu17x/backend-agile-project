267fc5b861a2c5a513e794f1ed2548e9277b2b9f
Eddu <azabacheeduardo85@gmail.com>
2025-10-22 18:51:32 -0500

    feat(email): add email service configuration and validation

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -4,3 +4,4 @@ export { default as cookieConfig } from './cookie.config';
 export { default as apiConfig } from './api.config';
 export { default as cloudflare } from './cloudflare.config';
 export { default as pinoLoggerConfig } from './pino-logger.config';
+export { default as emailConfig } from './mail';
