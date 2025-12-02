83147c1f743445cd4f317f1d0093fe649130fc6b
Eddu <azabacheeduardo85@gmail.com>
2025-12-01 22:02:20 -0500

    feat(clothes): add methods for adding and deleting images in clothes

--- src/clothes/clothes.controller.ts
+++ src/clothes/clothes.controller.ts
@@ -21,6 +21,8 @@ import { PresignedPut } from 'src/storage/interfaces/presigned-url.interface';
 import { UpdateClothesDTO } from './dto/update-clothes.dto';
 import { Variant } from './dto/variants.dto';
 import { UpdateVariantDTO } from './dto/update-variant.dto';
+import { AddImagesToClothesDTO } from './dto/add-images.dto';
+import { DeleteImageDTO } from './dto/delete-image.dto';
 
 @UseGuards(SuperTokensAuthGuard, RolesGuard)
 @Controller('clothes')
@@ -108,5 +110,29 @@ export class ClothesController {
     @Param('variantId', ParseUUIDPipe) variantId: string,
   ): Promise<any> {
     return this.clothesService.deleteVariant(clothesId, variantId);
+  }
+
+  @Roles(ROLES.ADMIN)
+  @Post(':id/images')
+  async addImages(
+    @Param('id', ParseUUIDPipe) clothesId: string,
+    @Body() addImagesDto: AddImagesToClothesDTO,
+  ): Promise<any> {
+    return this.clothesService.addNewImagesToClothes(
+      clothesId,
+      addImagesDto.images,
+    );
+  }
+
+  @Roles(ROLES.ADMIN)
+  @Delete(':id/images')
+  async deleteImage(
+    @Param('id', ParseUUIDPipe) clothesId: string,
+    @Body() deleteImageDto: DeleteImageDTO,
+  ): Promise<any> {
+    return this.clothesService.deleteImageFromClothes(
+      clothesId,
+      deleteImageDto.url,
+    );
   }
 }
