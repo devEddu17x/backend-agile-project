9305578f4a25ce7deddc0b271dbebfdef5892f99
Eddu <azabacheeduardo85@gmail.com>
2025-12-01 20:57:02 -0500

    feat(clothes): create UpdateClothesDTO for updating clothing items

---
+++ src/clothes/dto/update-clothes.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsString,
+  IsNumber,
+  Min,
+  IsBoolean,
+  IsOptional,
+} from 'class-validator';
+
+export class UpdateClothesDTO {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  price?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  isInEcommerce?: boolean;
+}
