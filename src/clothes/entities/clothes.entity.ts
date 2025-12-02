51b1721c33cf004ea7d6e53c625f8d7d60e393c1
Eddu <azabacheeduardo85@gmail.com>
2025-12-01 20:49:53 -0500

    feat(clothes): add isInEcommerce field to ClothesEntity

--- src/clothes/entities/clothes.entity.ts
+++ src/clothes/entities/clothes.entity.ts
@@ -23,6 +23,9 @@ export class ClothesEntity {
   @Column({ type: 'decimal', precision: 10, scale: 2, nullable: false })
   price: number;
 
+  @Column({ type: 'boolean', default: false })
+  isInEcommerce: boolean;
+
   @CreateDateColumn()
   createdAt: Date;
 
