fed62f76d7de2529bc11c4dfc186e4c27545c41f
Eddu <azabacheeduardo85@gmail.com>
2025-12-03 01:34:49 -0500

    refactor(quote): remove redundant id field from UpdateQuoteDTO

--- src/quote/dtos/update-quote.dto.ts
+++ src/quote/dtos/update-quote.dto.ts
@@ -1,12 +1,8 @@
 import { Type } from 'class-transformer';
-import { IsArray, IsNotEmpty, IsUUID, ValidateNested } from 'class-validator';
+import { IsArray, IsNotEmpty, ValidateNested } from 'class-validator';
 import { QuoteDetailDTO } from './create-quote.dto';
 
 export class UpdateQuoteDTO {
-  @IsNotEmpty()
-  @IsUUID()
-  id: string;
-
   @IsNotEmpty()
   @IsArray()
   @ValidateNested({ each: true })
