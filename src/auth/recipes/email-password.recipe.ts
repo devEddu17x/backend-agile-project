532bc385ee5c72a9780b72f4bdc3593ac1875e85
Eddu <azabacheeduardo85@gmail.com>
2025-12-04 10:59:03 -0500

    refactor(auth): remove unused signUp function and related user role assignment logic

--- src/auth/recipes/email-password.recipe.ts
+++ src/auth/recipes/email-password.recipe.ts
@@ -1,7 +1,4 @@
 import EmailPassword from 'supertokens-node/recipe/emailpassword';
-import SuperTokens from 'supertokens-node';
-import UserRoles from 'supertokens-node/recipe/userroles';
-import { ROLES } from '../constants/roles';
 import { ConfigService } from '@nestjs/config';
 import * as nodemailer from 'nodemailer';
 import { Template } from '../enums/email.template';
@@ -14,51 +11,6 @@ export function buildEmailPasswordRecipe(dependencies: {
   const transporter = nodemailer.createTransport(emailConfig.smtp);
 
   return EmailPassword.init({
-    override: {
-      functions: (orig) => ({
-        ...orig,
-        async signUp(input) {
-          const res = await orig.signUp(input);
-          if (res.status !== 'OK') {
-            return res;
-          }
-
-          // let appUser: EmployeeEntity | null = null;
-          try {
-            //
-            // appUser = await employeeService.createEmployee({
-            //   email: input.email,
-            // });
-            //
-            await UserRoles.addRoleToUser(
-              'public',
-              res.user.id,
-              ROLES.CUSTOMER,
-            );
-          } catch (error) {
-            SuperTokens.deleteUser(res.user.id);
-            throw error;
-          }
-          //
-          // if (!appUser) {
-          //   await SuperTokens.deleteUser(res.user.id);
-          //   throw new BadRequestException('Could not create user');
-          // }
-          //
-          // try {
-          //   await UserMetadata.updateUserMetadata(res.user.id, {
-          //     [APP_USER_ID_METADATA_KEY]: appUser.id,
-          //   });
-          // } catch (error) {
-          //   await employeeService.deleteEmployee(appUser.id);
-          //   await SuperTokens.deleteUser(res.user.id);
-          //   throw error;
-          // }
-
-          return res;
-        },
-      }),
-    },
     emailDelivery: {
       override: (orig) => ({
         ...orig,
