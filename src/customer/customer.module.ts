ad20303221717ae21a21cd1bb5e2adfdcfe28c2d
Eddu <azabacheeduardo85@gmail.com>
2025-12-04 11:33:43 -0500

    feat(customer): add CustomerEcommerceService to CustomerModule providers

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -3,10 +3,11 @@ import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerEntity } from './entities/customer.entity';
+import { CustomerEcommerceService } from './services/customer-ecommerce.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CustomerEntity])],
-  providers: [CustomerService],
+  providers: [CustomerService, CustomerEcommerceService],
   controllers: [CustomerController],
   exports: [CustomerService],
 })
